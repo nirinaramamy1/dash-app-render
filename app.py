a7e7476ed5fbd6092ed0746bb43409e5f955d17b
nirinaramamy1 <nirinaramamy1@gmail.com>
2025-06-11 15:52:17 +0300

    Dash minimal app

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 app = Dash()
 server = app.server
 
-# Requires Dash 2.17.0 or later
 app.layout = [
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
