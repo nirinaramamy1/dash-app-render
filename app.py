065c8e85a57d2adf236f02669cfd42a693007809
nirinaramamy1 <nirinaramamy1@gmail.com>
2025-06-11 15:11:58 +0300

    Next commit

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from dash import Dash, dcc, html, Input, Output, callback
-import os
+import dash
+from dash import html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
+server = app.server  # Important for Render
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div("Hello from Dash on Render!")
 
-server = app.server  # This is critical for Render (and Heroku)
-
-app.layout = html.Div([
-    html.H1('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'], 'LA', id='dropdown'),
-    html.Div(id='display-value')
-])
-
-@callback(Output('display-value', 'children'), Input('dropdown', 'value'))
-def display_value(value):
-    return f'You have selected {value}'
-
-if __name__ == '__main__':
-    app.run(debug=False)  # Set debug=False for production
+if __name__ == "__main__":
+    app.run_server(debug=True, host="0.0.0.0", port=10000)
