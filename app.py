bad77717826712cd515cce4632628073c277c1be
nirinaramamy1 <nirinaramamy1@gmail.com>
2025-06-13 23:59:02 +0300

    adding new graphs

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ on_off_head = html.H1("OnOff Data visualisation", className="bg-secondary text-w
 singer_head = html.H2("Singers visualisation", className="bg-secondary text-white p-2")
 singer_project_head = html.H2("Singers interaction with projects visualisation", className="bg-secondary text-white p-2")
 
-
 def fetch_data(query):
     cursor.execute(query)
     results = cursor.fetchall()
@@ -286,6 +285,12 @@ def project_genres_graph():
         )
     ], className="mt-2 mb-2")
 
+singers = fetch_data("""
+    SELECT name
+    FROM singers
+    WHERE name <> 'scraper'
+""")["name"]
+
 app.layout = dbc.Container(
     [
         on_off_head,
@@ -299,10 +304,87 @@ app.layout = dbc.Container(
             dbc.Col(project_per_language()),
             dbc.Col(project_per_song_type()),
             dbc.Col(project_genres_graph()),
+            dbc.Row([
+                dbc.Col(
+                    dbc.Card([
+                        dbc.CardHeader([
+                            html.H2("Projets par langue pour le chanteur", className="text-center"),
+                        ]),
+                        dbc.CardBody([
+                            dbc.Row([
+                                    dcc.Dropdown(
+                                    id='singer-dropdown',
+                                    options=[{'label': singer, 'value': singer} for singer in singers],
+                                    value=singers[0],
+                                ),
+                            ]),
+                            dbc.Row(id='singer-projects-graph')
+                        ])
+                    ]))
+            ], className="mt-2 mb-2", align="center")
         ]),
     ],
     fluid=True,
 )
+
+@app.callback(
+    Output('singer-projects-graph', 'children'),
+    Input('singer-dropdown', 'value')
+)
+def singer_projects_by_language_graph(selected_singer):
+    df = fetch_data("""
+        SELECT
+            s.name,
+            po.title,
+            po.language
+        FROM
+            singers s
+        JOIN
+            project_singer_association psa ON s.id = psa.singer_id
+        JOIN
+            project_observations po ON psa.project_observation_id = po.id
+        WHERE s.is_active = po.is_active AND s.name <> 'scraper'
+    """)
+    df = df.groupby(['name', 'language'])['title'].count().reset_index()
+    df.columns = ['name', 'language', 'project_count']
+    # df['language_label'] = df['language'].apply(
+    #     lambda lang: f"{lang} ({df.groupby('language')['project_count'].sum().to_dict()[lang]})"
+    # )
+    df = df[df['name'] == selected_singer]
+
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                x=df['language'],
+                # x=df['language_label'],
+                y=df['project_count'],
+                text=df['project_count'],
+                marker=dict(
+                    color=list(range(len(df['language'].values))),
+                )
+            )
+        ]
+    )
+
+    fig.update_layout(
+        title=f"Projets interprétés par {selected_singer  if selected_singer else 'No One'}",
+        xaxis_title='Langue',
+        # xaxis_title='Langue (total global)',
+        yaxis_title='Nombre de projets',
+        annotations=[
+            dict(
+                text=f"Total: {df['project_count'].sum()}",
+                x=1,
+                y=1.1,
+                xref='paper',
+                yref='paper',
+                showarrow=False,
+                font=dict(size=14),
+                align='right'
+            )
+        ]
+    )
+    return dcc.Graph(figure=fig, config={'responsive': False})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=10000)
